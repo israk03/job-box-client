af99dc0aa57cd92a0ee02c3c48d94bd3c7241de4
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2025-05-27 23:19:33 +0600

    footer added

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
+import Footer from "@/components/Footer/Footer";
 
 export default function MainLayout() {
   return (
     <div className="max-w-7xl mx-auto ">
       <Navbar></Navbar>
       <Outlet />
+      <Footer></Footer>
     </div>
   );
 }
